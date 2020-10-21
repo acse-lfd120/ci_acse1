33821528e81de9e2e367dfdad8c7116622c92ec4
Lawrence Dior <lfd120@ic.ac.uk>
2020-10-21 18:58:06 +0100

    Added a new function, do_something_with_pandas.
    This introduces a new dependency on the pandas package.
    Added a test for do_something_with_pandas.

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
+import pandas as pd
 
-__all__ = ['my_sum', 'factorial']
+__all__ = ['my_sum', 'factorial', 'do_something_with_pandas']
 
 
 def my_sum(iterable):
@@ -13,3 +14,9 @@ def my_sum(iterable):
 @lru_cache(maxsize=None)
 def factorial(n):
     return n * factorial(n-1) if n else 1
+
+
+def do_something_with_pandas(x):
+    return pd.DataFrame({str(x): [x*2, x*3],
+                         str(x[0]): [x[0]*2, x[0]*3]},
+                        index=['double', 'triple'])
--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -1,6 +1,7 @@
 import pytest
 
-from simple_functions import my_sum, factorial
+from simple_functions import my_sum, factorial, \
+                             do_something_with_pandas
 
 
 class TestSimpleFunctions(object):
@@ -23,4 +24,20 @@ class TestSimpleFunctions(object):
     def test_factorial(self, number, expected):
         '''Test our factorial function'''
         answer = factorial(number)
+        assert answer == expected
+
+    @pytest.mark.parametrize('x, expected', [
+        ([1, 2, 3],
+         [[1, 2, 3, 1, 2, 3],
+          [1, 2, 3, 1, 2, 3, 1, 2, 3],
+          2, 3]),
+        ('abc', ['abcabc', 'abcabcabc', 'aa', 'aaa'])
+    ])
+    def test_do_something_with_pandas(self, x, expected):
+        '''Test our do_something_with_pandas function'''
+        df = do_something_with_pandas(x)
+        answer = [df[str(x)].double,
+                  df[str(x)].triple,
+                  df[str(x[0])].double,
+                  df[str(x[0])].triple]
         assert answer == expected
